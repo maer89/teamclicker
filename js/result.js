6e2c108259dcf02f8e96634a2ffb9ba9638396b4
Marci <marci.erath@web.de>
2013-04-29 19:49:25 +0200

    Chart Tool eingebaut

--- js/result.js
+++ js/result.js
@@ -1,4 +1,14 @@
+var ans1 = 15;
+var ans2 = 6;
+var ans3 = 5;
+var ans1text;
+var ans2text;
+var numb = 0;
+
 function connect(){
+//google.load('visualization', '1.0', {'packages':['corechart']});
+//google.setOnLoadCallback(drawChart);
+	
 	var id = storage.get("q_id");
 	//storage.set("q_id", "");
 	$.ajax({
@@ -10,9 +20,13 @@ function connect(){
 		success : function(response) {
 			var data_field = $.parseJSON(response);
 			$("#result").empty();
-			
 			if(data_field.num == 2){
 				$("#result").append("<p>" + data_field.ans1text + ": " + data_field.ans1 + "</p><p>" + data_field.ans2text + ": " + data_field.ans2 + "</p>");
+				ans1 = data_field.ans1;
+				ans2 = data_field.ans2;
+				ans1text = data_field.ans1text;
+				ans2text = data_field.ans2text;
+				numb = 2;
 			}else if(data_field.num == 3){
 				$("#result").append("<p>" + data_field.ans1text + ": "+ data_field.ans1 + "</p><p>" + data_field.ans2text + ": "  + data_field.ans2 + "</p><p>" + data_field.ans3text + ": " + data_field.ans3 + "</p>");
 			}else if(data_field.num == 4){
@@ -27,6 +41,7 @@ function connect(){
 			}else{
 				//do nothing
 			}
+			
 		},
 		complete : function(response) {
 			connect();
